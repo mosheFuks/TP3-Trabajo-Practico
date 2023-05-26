628e89fc18f75cdb74be1dae968515991583cb2a
Elyelin Carrasquero <elyelin15@gmail.com>
2023-05-26 18:36:51 -0300

    navigate drawer

--- app/src/main/java/com/example/trabajopractico/MainActivity.kt
+++ app/src/main/java/com/example/trabajopractico/MainActivity.kt
@@ -2,14 +2,73 @@ package com.example.trabajopractico
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.MenuItem
+import android.widget.Toast
+import android.widget.Toolbar
+import androidx.appcompat.app.ActionBarDrawerToggle
+import androidx.core.view.GravityCompat
+import androidx.drawerlayout.widget.DrawerLayout
 import androidx.navigation.findNavController
 import androidx.navigation.ui.AppBarConfiguration
 import androidx.navigation.ui.setupActionBarWithNavController
+import com.example.trabajopractico.framents.fragment_home
+import com.example.trabajopractico.framents.fragment_list
+import com.google.android.material.navigation.NavigationView
 
-class MainActivity : AppCompatActivity() {
+class MainActivity : AppCompatActivity(), NavigationView.OnNavigationItemSelectedListener {
+
+    private lateinit var drawerLayout: DrawerLayout
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val toolbar: androidx.appcompat.widget.Toolbar = findViewById(R.id.toolbar)
+        setSupportActionBar(toolbar)
+
+        drawerLayout = findViewById(R.id.drawer_layout)
+
+        val navigationView: NavigationView = findViewById(R.id.navigation_drawer)
+        navigationView.setNavigationItemSelectedListener(this)
+
+        val toggle = ActionBarDrawerToggle(this, drawerLayout, toolbar, R.string.open_nav, R.string.close_nav)
+
+        drawerLayout.addDrawerListener(toggle)
+        toggle.syncState()
+
+        if (savedInstanceState == null) {
+            supportFragmentManager.beginTransaction()
+                .replace(R.id.fragment_container, fragment_home())
+                .commit()
+        }
+    }
+    override fun onNavigationItemSelected(item: MenuItem): Boolean {
+        when (item.itemId) {
+
+            R.id.nav_fragment_setting -> supportFragmentManager.beginTransaction()
+                .replace(R.id.fragment_container, fragment_setting())
+                .commit()
+
+            R.id.nav_fragment_profile -> supportFragmentManager.beginTransaction()
+                .replace(R.id.fragment_container, fragment_profile())
+                .commit()
+
+            R.id.nav_fragment_list-> supportFragmentManager.beginTransaction()
+                .replace(R.id.fragment_container, fragment_list())
+                .commit()
+        }
+
+        drawerLayout.closeDrawer(GravityCompat.START)
+        return true
+    }
+
+    override fun onBackPressed() {
+        if (drawerLayout.isDrawerOpen(GravityCompat.START)) {
+            drawerLayout.closeDrawer(GravityCompat.START)
+        } else {
+            super.onBackPressed()
+        }
     }
 
 }
+
+
