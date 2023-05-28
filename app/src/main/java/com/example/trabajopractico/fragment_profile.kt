18f832cae440d2e2da46df4fbd1e9f1919743310
mosheFuks <moshifuks2001@gmail.com>
2023-05-28 20:42:27 -0300

    Loguin added

--- app/src/main/java/com/example/trabajopractico/fragment_profile.kt
+++ app/src/main/java/com/example/trabajopractico/fragment_profile.kt
@@ -1,6 +1,7 @@
 package com.example.trabajopractico
 
 import android.os.Bundle
+import android.util.Log
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
@@ -35,6 +36,11 @@ class fragment_profile : Fragment() {
     ): View? {
         // Inflate the layout for this fragment
         return inflater.inflate(R.layout.fragment_profile, container, false)
+
+        //Obtener el nombre del usuario
+        val dataBundle = arguments
+        val value = dataBundle!!.getString("userName")!!
+
     }
 
     companion object {
