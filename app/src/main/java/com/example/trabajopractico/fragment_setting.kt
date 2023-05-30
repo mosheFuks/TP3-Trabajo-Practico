ee702cb1558a8a39c6afe8919facb1fe8fd7df51
Elyelin Carrasquero <elyelin15@gmail.com>
2023-05-30 17:02:48 -0300

    fragmento de configuracion

--- app/src/main/java/com/example/trabajopractico/fragment_setting.kt
+++ app/src/main/java/com/example/trabajopractico/fragment_setting.kt
@@ -5,15 +5,32 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-
+import android.widget.Switch
+import androidx.appcompat.app.AppCompatActivity
 
 
 class fragment_setting : Fragment(R.layout.fragment_setting) {
 
-    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
-        super.onViewCreated(view, savedInstanceState)
+    private lateinit var switchNightMode: Switch
+    private lateinit var switchNotifications: Switch
+    override fun onCreateView(
+            inflater: LayoutInflater, container: ViewGroup?,
+            savedInstanceState: Bundle?
+        ): View? {
+            val view = inflater.inflate(R.layout.fragment_setting, container, false)
 
+            switchNightMode = view.findViewById(R.id.switchNightMode)
+            switchNotifications = view.findViewById(R.id.switchNotifications)
 
+           /* // lógica para manejar los cambios en los Switches
+            switchNightMode.setOnCheckedChangeListener { _, isChecked ->
+                // Lógica para cambiar al modo nocturno
+            }
+
+            switchNotifications.setOnCheckedChangeListener { _, isChecked ->
+                // Lógica para activar/desactivar las notificaciones
+            }*/
+
+            return view
+        }
     }
-
-}
