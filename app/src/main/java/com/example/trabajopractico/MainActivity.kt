a085a8e231ba3532072a78f3d5896960b3c584ef
mosheFuks <moshifuks2001@gmail.com>
2023-05-30 20:17:25 -0300

    Navegacion lateral

--- app/src/main/java/com/example/trabajopractico/MainActivity.kt
+++ app/src/main/java/com/example/trabajopractico/MainActivity.kt
@@ -3,26 +3,43 @@ package com.example.trabajopractico
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.util.Log
+import android.widget.FrameLayout
+import android.widget.ImageView
+import android.widget.TextView
+import androidx.core.view.GravityCompat
+import androidx.drawerlayout.widget.DrawerLayout
 import androidx.fragment.app.Fragment
 import androidx.navigation.findNavController
 import androidx.navigation.ui.AppBarConfiguration
+import androidx.navigation.ui.NavigationUI
 import androidx.navigation.ui.setupActionBarWithNavController
+import androidx.navigation.ui.setupWithNavController
 import com.example.trabajopractico.databinding.ActivityMainBinding
+import com.google.android.material.navigation.NavigationView
 
 class MainActivity : AppCompatActivity() {
 
     private lateinit var binding : ActivityMainBinding
 
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         val userName = intent.getStringExtra("userName")
         binding = ActivityMainBinding.inflate(layoutInflater)
+        val drawerLayout = binding.drawerLayoutView
+        //val navigationView: NavigationView = findViewById(R.id.navigationView)
         setContentView(binding.root)
         replaceFragment(fragment_home())
 
+        val slideMenu = findViewById<ImageView>(R.id.menu_slider)
+        val navigationView = findViewById<NavigationView>(R.id.navigationView)
+        slideMenu.setOnClickListener {
+            navigationView.getHeaderView(0).findViewById<TextView>(R.id.txtHeaderUserName).text = userName.toString();
+            drawerLayout.openDrawer(GravityCompat.START)
+        }
+
 
         binding.bottomNavigationView.setOnItemSelectedListener {
-
             when(it.itemId){
                 R.id.home -> replaceFragment(fragment_home())
                 R.id.autos -> replaceFragment(fragment_list())
@@ -36,6 +53,7 @@ class MainActivity : AppCompatActivity() {
                 R.id.profile -> replaceFragment(fragment_profile(), userName.toString())
                 R.id.setting -> replaceFragment(fragment_setting())
             }
+            drawerLayout.closeDrawer(GravityCompat.START)
             true
         }
     }
@@ -49,8 +67,6 @@ class MainActivity : AppCompatActivity() {
             var dataBundle = Bundle()
             dataBundle.putString("userName", userData);
             fragment.arguments = dataBundle
-            val headerFragment = nav_header();
-            headerFragment.arguments = dataBundle
         }
 
         fragmentTransaction.replace(R.id.frame_layout,fragment)
