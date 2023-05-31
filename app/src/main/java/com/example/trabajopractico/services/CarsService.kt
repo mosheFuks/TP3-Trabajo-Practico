f22ad9b629015b9aa22b57c304a72b5e6f46868c
JuanCampagna <campagnajuanmanuel@gmail.com>
2023-05-31 00:29:32 -0300

    se pone más límite de autos, se cambia el logo del activity_main, se pulen los estilos del listado de autos

--- app/src/main/java/com/example/trabajopractico/services/CarsService.kt
+++ app/src/main/java/com/example/trabajopractico/services/CarsService.kt
@@ -7,6 +7,6 @@ import retrofit2.http.Headers
 
 interface CarsService {
     @Headers("X-Api-Key: 8rzv6uDX8hCMD/cH1nVv+g==SyC9W1j1RLVWQ8FH")
-    @GET("cars?limit=3&fuel_type=gas")
+    @GET("cars?limit=10&fuel_type=gas")
     fun getCars(): Call<List<Car>>
 }
